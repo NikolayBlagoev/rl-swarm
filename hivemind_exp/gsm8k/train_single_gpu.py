5a1b5632f794e7fe4a832a651157f8250703dbbb
NikolayBlagoev <workerthreadllc@gmail.com>
2025-04-01 15:39:33 +0300

    i am blind

--- hivemind_exp/gsm8k/train_single_gpu.py
+++ hivemind_exp/gsm8k/train_single_gpu.py
@@ -53,8 +53,8 @@ def get_model(args: GRPOConfig, model_name: str):
     model_init_kwargs["use_cache"] = (
         False if args.gradient_checkpointing else model_init_kwargs.get("use_cache")
     )
-    print("USING ",**model_init_kwargs)
-    return AutoModelForCausalLM.from_pretrained(model_name, model_init_kwargs)
+    print("USING ",model_init_kwargs)
+    return AutoModelForCausalLM.from_pretrained(model_name, **model_init_kwargs)
 
 
 def get_tokenizer_name(model_args: ModelConfig, script_args: ScriptArguments):
